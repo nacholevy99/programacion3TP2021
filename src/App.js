429bb69032d8c30e83a11dfe7228a9d321e6b48d
nacholevy99 <juanignaciolevy@gmail.com>
2021-09-09 17:01:57 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 // Aca vamos a ir importando los componentes y todo lo que queramos usar en este archivo
-import "./App.css"
+import React from "react";
+import reactDom from "react-dom";
+import "./App.css";
 import Movies from "./Components/Movies/Movies";
 
 //Aca vamos a ir agregnado todos los componentes para visualizarlos en la pagina
 function App() {
   return (
-    <div className="home">
-      <header className="page-header">
-        <p>Proyecto Integrador Holaaa</p>
+    <React.Fragment>
+      <header>
+        <h1>Título/ Nombre de la app</h1>
+        <section>
+          <p>Ordenar ASC/ DESC</p>
+          <i className="fas fa-th"></i>
+          <i className="fas fa-align-justify"></i>
+          <form action="">
+            <input type="text" name="search" id="" placeholder="Search" />
+            <button type="submit">
+              <i className="fas fa-search"></i>
+            </button>
+          </form>
+        </section>
       </header>
-      <div>
-        <Movies />
+      <main>
+        <button type="button">Cargar más tarjetas</button>
+        <section className="card-container">
+          <article>
+            <section className="navigation">
+              <div>
+                <i className="fas fa-chevron-left"></i>
+                <i className="fas fa-chevron-right"></i>
+              </div>
+              <i className="far fa-window-close"></i>
+            </section>
+          </article>
+        </section>
+      </main>
+      <footer>
+        <ul className="team">
+          <li>Nombre integrante 1</li>
+          <li>Nombre integrante 2</li>
+          <li>Nombre integrante 3</li>
+        </ul>
+      </footer>
+      <div classNameName="home">
+        <div>
+          <Movies />
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 }
 
