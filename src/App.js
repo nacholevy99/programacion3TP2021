22cc570a9670dd81a62ab9f5466770e32bf39930
nacholevy99 <juanignaciolevy@gmail.com>
2021-09-09 17:03:29 -0300

    borro maqueta

--- src/App.js
+++ src/App.js
@@ -11,9 +11,6 @@ function App() {
       <header>
         <h1>Título/ Nombre de la app</h1>
         <section>
-          <p>Ordenar ASC/ DESC</p>
-          <i className="fas fa-th"></i>
-          <i className="fas fa-align-justify"></i>
           <form action="">
             <input type="text" name="search" id="" placeholder="Search" />
             <button type="submit">
